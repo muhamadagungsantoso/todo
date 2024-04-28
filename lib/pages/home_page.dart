1b462744747995cef61467b3d23887a7097a764d
Muhamad Agung Santoso <muhamadagung70674@gmail.com>
2024-04-28 21:47:22 +0700

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:todo/utility/todo_tile.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List todoList = [
+    ["Wake up in the morning!", false],
+    ["Take a shower", false]
+  ];
+
+  // checkbox was tapped
+  checkBoxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = !todoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue.shade100,
+      appBar: AppBar(
+        title: const Text(
+          "To Do",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: todoList[index][0],
+            taskCompleted: todoList[index][1],
+            onChanged: (p0) => checkBoxChanged(p0, index),
+          );
+        },
+      ),
+    );
   }
 }
